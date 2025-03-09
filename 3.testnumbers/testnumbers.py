05ceeee37ccab9b2dbb59fd47ad6cb55de53acdf
fuck_if <wongjy13005151320@outlook.com>
2025-03-10 00:30:50 +0800

    增加算术运算篇章

---
+++ 3.testnumbers/testnumbers.py
@@ -0,0 +1,24 @@
+print("算数运算符")
+x = 12
+y = 13
+z = 0
+z = x + y
+print("1 z = ", z)
+z += 1
+print("2 z = ", z)
+z = y - x
+print("3 z = ", z)
+z -= 3
+print("4 z = ", z)
+y = 3
+x = 2
+z = y / x
+print("5 z = ", z)
+
+y = 9
+x = 2
+print("6 z = ", y % x) #取模（去余数）
+
+print("6 z = ", y // x)
+
+print(" 2 ** 4 =", 2 ** 10)
