f6bb61c5d296b05458c43502d19e266f29d928f8
fuck_if <wongjy13005151320@outlook.com>
2025-03-14 00:26:06 +0800

    #r或R 转义成为普通字符串 #字符串转义 #多行字符串 #字符串的拼接和格式化 #运行时进行拼接

--- 1-pybase/4.teststring.py
+++ 1-pybase/4.teststring.py
@@ -13,3 +13,37 @@ print("str1[-3:-1] = ",str1[-3:-1]) #负数从结尾开始,范围[-3,-1)
 print("str1[:-2] = ",str1[:-2]) #去掉最后字符,不指定开始位置
 str1 = "file.mp4"
 print("str1[-3:] = ",str1[-3:]) #不指定结束位置
+
+#字符串转义
+print("AAAAAAAAA\n\\\"BBBBBBBBB")
+print("'CCCCCCCCCCCCCCCCCCCCCCCCC'")
+print('"CCCCCCCCCCCCCCCCCCCCCCCCC"')
+
+#r或R 转义成为普通字符串
+print(r"DDD\tDDDD\n\\\"EEEE\tEEEE")
+
+#多行字符串
+a = "11111111111111" \
+    "2222222222222"  \
+    "3333333333333"
+print(a)
+a = "11111111111111 \
+    2222222222222  \
+    3333333333333"
+print(a)
+a = """
+    aaaaaaaaaaaaaaa
+    bbbbbbbbbbbbbbb
+    ccccccccccccccc
+"""
+print(a)
+
+#字符串的拼接和格式化
+#运行时进行拼接
+s1 = "1234567890一二三四五"
+s2 = "YYYYYYYYYYYYYYYY"
+s3 = s1 + s2 + " " + str(123)+ " " + str(100.9)
+print(s3)
+# %s 字符串 %d 整数 %c 字符 %f浮点数
+s4 = "格式化字符串 %s %d %c %c %c %f  size is %d" % ("teststring", 300, s1[0], 65, chr(66), 10.9, len(s1))
+print(s4)
