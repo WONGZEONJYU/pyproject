5f4dccd8737d3f5d87551282ff59c7feebd7382d
fuck_if <wongjy13005151320@outlook.com>
2025-03-18 22:55:41 +0800

    第五章，列表的增删改查

---
+++ 1-pybase/5.testlist.py
@@ -0,0 +1,24 @@
+str4 = "str004"
+i1 = 5
+list1 = ["str001", "str002", "str003", str4, i1]
+print(list1)
+print(type(list1))
+print(id(list1))
+print(len(list1))
+list2 = ["str01", "str02", [1,2,3] ]
+print(list2)
+print(list1[2])
+print(type(list1[2]))
+print(list2[2][2])
+
+list1[0] = 1001
+print(list1)
+list1.insert(1, "xcj") #插入数据 O(n)
+print(list1)
+list1.append("dst") #O(1)
+print(list1)
+print(list2[-2]) #负数从最后开始往前
+list1.pop(-1) #清理最后一个元素
+print(list1)
+list1.pop(2) #清理最后一个元素
+print(list1)
