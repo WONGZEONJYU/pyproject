8ffd54e9a958e3466ac703bd67d5c8688b3861eb
fuck_if <wongjy13005151320@outlook.com>
2025-03-20 00:08:57 +0800

    增加字典章节

---
+++ 1-pybase/6.testdic.py
@@ -0,0 +1,31 @@
+print("Test Dictionary")
+# {"key1":value1, "key2",value2 } key需要计算hash值 value可以装任意类型
+# dictionary 初始化
+dic = {"name": "Python",
+       "year":1991,
+       "ver":3.7,
+       4:5,
+       "keys":["print","for"]}
+print(dic)
+# 新增和更改
+dic[4] = 6
+dic["src"] = "C"
+print(dic)
+# 删除
+del dic[4]
+dic.pop("keys")
+print(dic)
+
+
+# 判断关键字
+try:
+    print(dic["name"])
+    print(dic["arg"])
+except KeyError:
+    print("dic KeyError ")
+
+if "year" in dic:
+    print("key year in dic")
+
+
+dic.clear()
